0afa86c3b2ea0c6b199d6d5c008c3c50717be94e
Julio <juliodesoto@alumnos.cei.es>
2025-02-13 11:35:10 +0100

    parámetros URL validados

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ servidor.post("/tareas/nueva", (peticion,respuesta) => {
     respuesta.send("POST/tareas/nueva");
 });
 
-servidor.delete("/tareas/borrar/:id", (peticion,respuesta) => {
+servidor.delete("/tareas/borrar/:id([0-9]+)", (peticion,respuesta) => {
     respuesta.send("DELETE/tareas/borrar/:id");
 });
 
-servidor.put("/tareas/editar/texto/:id", (peticion,respuesta) => {
+servidor.put("/tareas/editar/texto/:id([0-9]+)", (peticion,respuesta) => {
     respuesta.send("PUT/tareas/editar/texto/:id");
 });
 
-servidor.put("/tareas/editar/estado/:id", (peticion,respuesta) => {
+servidor.put("/tareas/editar/estado/:id([0-9]+)", (peticion,respuesta) => {
     respuesta.send("PUT/tareas/editar/estado/:id");
 });
 
